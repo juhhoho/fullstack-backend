24a1c7b831c70962b95c6cfd4beda3dfb3ec5a3f
junho <jh102328@khu.ac.kr>
2025-05-10 00:40:56 +0900

    update distance threshold

--- src/main/java/_2025/fullstack/backend/domain/progress/service/ProgressQueryService.java
+++ src/main/java/_2025/fullstack/backend/domain/progress/service/ProgressQueryService.java
@@ -56,7 +56,7 @@ public class ProgressQueryService {
             landmarkList.add(
                     new LandmarkBriefDto(
                             landmark.getName(),
-                            isWithinDistance(latitude, longitude, landmark.getLatitude(), landmark.getLongitude(), 400)
+                            isWithinDistance(latitude, longitude, landmark.getLatitude(), landmark.getLongitude(), 1_000_000)
                     )
             );
         }
