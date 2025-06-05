871caa22e4f3fd84200f3247af99cd02dfea0f5e
junho <jh102328@khu.ac.kr>
2025-06-05 23:05:06 +0900

    fix health check

--- src/main/java/_2025/fullstack/backend/domain/user/controller/HealthCheck.java
+++ src/main/java/_2025/fullstack/backend/domain/user/controller/HealthCheck.java
@@ -2,6 +2,7 @@ package _2025.fullstack.backend.domain.user.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class HealthCheck {
 
-    @GetMapping("/healthCheck")
-    public String healthCheck(){
-        return "health-check";
+    @GetMapping("/health")
+    public ResponseEntity<String> healthCheck(){
+        return ResponseEntity.ok().body("eraijasicka");
     }
 }
